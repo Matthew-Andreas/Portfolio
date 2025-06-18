301c2e659cd217ca62e4a0f11a513d0e4b34d49d
Matthew-Andreas <matthew66andreas@gmail.com>
2025-06-18 11:38:15 -0700

    Finished a couple more text segements for the fire and smoke detection page.

--- src/pages/FaSDwYOLOPage.tsx
+++ src/pages/FaSDwYOLOPage.tsx
@@ -75,7 +75,26 @@ const FaSDwYOLOPage = () => {
             90's in some areas but we were not able recreate these results even
             using their exact specifications in their paper.
           </p>
-          <p>Explain what each column means</p>
+          <p>
+            There are 4 attributes for a model that we look at to see how
+            accurate the model is at correctly determining whether there is
+            fire/smoke or not in an image and where it is in the image. The
+            first attribute is precision which focuses on false postives which
+            in this case would be the model saying there is fire/smoke when
+            there is not one in the image. The next attribute is recall which
+            focuses on false negatives which is where the model says there is no
+            fire/smoke in the image when there actually is. This is the
+            attribute we focused on improving the most since if a fire was not
+            detected it could lead to a bigger fire that cause more damages and
+            greater loss of life. Then we have the attribute mAP:50 which uses
+            both precision and recall, averaged across the 3 classes of fire,
+            smoke and background, while using a fixed IoU threshold of
+            0.50(i.e., there is a 50% overlap between the predicted and true
+            boxes which you can see in the above image). Lastly we got the
+            attribute mAP:50-95 which is the same as the last but goes through
+            IoU threshold .50 to .95 in .05 increments and averages them
+            together.
+          </p>
         </div>
         <div className="ImgContainer">
           <img className="aboutImg" src="./yoloOGPaperResults.png" alt="" />
@@ -84,7 +103,13 @@ const FaSDwYOLOPage = () => {
       <div className="about right">
         <div className="aboutText">
           <h1>Our Implementation of the Paper's Method</h1>
-          <p></p>
+          <p>
+            For this project we reimplement the paper's method and after
+            following every parameter in the paper we got these results for
+            YOLOv8 models. For our results you can see that the YOLOv8s does the
+            best in most of the metrics and YOLOv8x got the best in the
+            precision metric.
+          </p>
         </div>
         <div className="ImgContainer">
           <img className="aboutImg" src="./OurPaperImplementation.png" alt="" />
@@ -93,7 +118,21 @@ const FaSDwYOLOPage = () => {
       <div className="about left">
         <div className="aboutText">
           <h1>Comparison Between Our Implementation and the Paper's</h1>
-          <p></p>
+          <p>
+            As we compare our implementation to the paper's we can see that
+            there is a big difference in the metrics but we followed every
+            parameter in the paper and still ended up with the lower scores.
+            However, across all of our alternative methods we seemed to be
+            relatively close with our implementation's metrics and we never saw
+            the scores as high as the orginal paper. So we decide to compare the
+            alternative methods metrics to our reimplementation of the paper's
+            method instead of the orignal metrics. Another thing you can see
+            when comparing these metrics is that for their models YOLOv8x did
+            the best in every category but in our data YOLOv8s did the best in
+            most categories which we found very interesting. This trend of small
+            models doing better then the larger ones can also be seen through
+            out the rest of our methods.
+          </p>
         </div>
         <div className="ImgContainer">
           <img className="aboutImg" src="./PaperVSOurImp.png" alt="" />
@@ -104,7 +143,14 @@ const FaSDwYOLOPage = () => {
           <h1>
             Alternative Method Using Transfer Learning(TL) on YOLOv8 Models
           </h1>
-          <p></p>
+          <p>
+            This is our first alternative method's results. For this method we
+            used the same base models as the paper but removed the last layer of
+            the model or the head and replaced with a custmized layer for this
+            specific problem. We also froze all of the layers execept the new
+            head for the first 10 epochs of training to train the new head and
+            then unfroze all the layers for another 290 epoch.
+          </p>
         </div>
         <div className="ImgContainer">
           <img className="aboutImg" src="./YoloTLResults.png" alt="" />
@@ -116,7 +162,17 @@ const FaSDwYOLOPage = () => {
             Comparison Between Transfer Learning and Our Implementation of the
             Paper's Method
           </h1>
-          <p></p>
+          <p>
+            As we compare our first alternative method to the reimplementation
+            of the paper's method we can see in the metrics recall, mAP:50, and
+            mAP:50-95 were better in the new alternative method than the old
+            method. However, for percision even though we had many of the models
+            perform better in the new method none of the beat the highest
+            precision in the old method. Also you can see that for the highest
+            of each column they didn't increase by too much since at most one
+            increased by .012, that column was recall which was our focus. So we
+            didn't see too much of an improvement.
+          </p>
         </div>
         <div className="ImgContainer">
           <img className="aboutImg" src="./CompTLVSOurImp.png" alt="" />
@@ -125,7 +181,14 @@ const FaSDwYOLOPage = () => {
       <div className="about right">
         <div className="aboutText">
           <h1>Alternative Method Using YOLOv12 Models</h1>
-          <p></p>
+          <p>
+            This is our second alternative where we used the YOLOv12 models with
+            all the same metrics from the paper to see if the newer versions of
+            these models were better. The interesting thing we found with this
+            data is that the highest metrics in each columns seem to be in
+            different models execept for percision and mAP:50 which were both in
+            the YOLOv8n.
+          </p>
         </div>
         <div className="ImgContainer">
           <img className="aboutImg" src="./Yolov12.png" alt="" />
@@ -153,31 +216,6 @@ const FaSDwYOLOPage = () => {
         </div>
         <div className="ImgContainer">
           <img className="aboutImg" src="./Comp12VSTL.png" alt="" />
-        </div>
-      </div>
-      <div className="about left">
-        <div className="aboutText">
-          <h1>Level 5</h1>
-          <p></p>
-        </div>
-        <div className="ImgContainer">
-          <img className="aboutImg" src="./RRLevel5.png" alt="" />
-        </div>
-      </div>
-      <div>
-        <div className="about left">
-          <div className="aboutText">
-            <h1>Main Hub</h1>
-            <p></p>
-          </div>
-          <div className="ImgContainer">
-            <p>Training</p>
-            <img className="aboutImg" src="./yolov8nTraining.png" alt="" />
-            <p>Testing</p>
-            <img className="aboutImg" src="./yolov8nTesting.png" alt="" />
-            <img className="aboutImg" src="./yolov8nConfus.png" alt="" />
-            <img className="aboutImg" src="./yolov8nRecall.png" alt="" />
-          </div>
         </div>
       </div>
       <div id="Links" className="links left">
